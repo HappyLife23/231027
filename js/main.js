c5d64f3d883299c995223d69c81083c219e8cacd
Jens Bengtsson <jb225je@student.lnu.se>
2023-10-27 15:27:44 +0200

    all js and some minor other uppdates

--- js/main.js
+++ js/main.js
@@ -1,31 +1,42 @@
-const nrOfPpl = document.getElementById("nrOfPpl"); // antal personer
+const submitBtn = document.getElementById("submit");
+submitBtn.addEventListener("click", function() {
+    makeMenu(e);
+});
 
-
-
-
-
-// meddelande
-
+const todaysLunch = document.querySelector(".todays-lunch1"); // välkomns meddelande i todays-lunch sectionen
 const nameOfCompany = document.getElementById("nameOfCompany"); // input för company name
-const welcome = document.querySelector(".welcome"); // välkomns meddelande
-welcome.innerHTML += `<br> ${nameOfCompany.value}`;
-
-// ----------------------------------
-
+const nrOfPpl = document.getElementById("nrOfPpl"); // antal personer
 const radioButton = document.getElementsByName("typeOfMeal"); // referens till radio knappen
 
-let selectedValue = ""; // sparar värdet från radioknappen (meat eller fish)
-for (let i = 0; i < radioButton.length; i++) { // loopar igenom arrayen radioButton
-    if (radioButton[i].checked) { // kollar om radio button är checked
-      selectedValue = radioButton[i].value;
-      break; // Bryt loopen när en markerad knapp hittas
+function makeMenu(e) {
+    e.preventDefault();
+    createAndPrint("h2", "Thank you and welcome! <br>", nameOfCompany.value, "");
+    createAndPrint("p", "You are ", nrOfPpl.value, " persons in your company.");
+
+
+    let selectedValue = ""; // sparar värdet från radioknappen (meat eller fish)
+    for (let i = 0; i < radioButton.length; i++) { // loopar igenom arrayen radioButton
+        if (radioButton[i].checked) { // kollar om radio button är checked
+            selectedValue = radioButton[i].value;
+            break; // Bryt loopen när en markerad knapp hittas
+        }
     }
+    createAndPrint("p", "Your choosed to eat ", selectedValue, " today!");
 }
 
-const lunchHeader = document.getElementsByClassName("lunch-header");
-lunchHeader.innerHTML = `<br> ${selectedValue}`;
+function createAndPrint(elem, text, elemRef, textEnd) {
+    const newElem = document.createElement(elem);
+    newElem.innerHTML = text + elemRef + textEnd
+    todaysLunch.appendChild(newElem);
+}
 
-console.log(selectedValue + " selected value");
+// let selecetedAdditions = [];
+// const additions = document.getElementsByName("additions");
+// for (let i = 0; i < additions.length; i++) {
+//     if (additions[i].checked) {
+//         selecetedAdditions.push(additions[i].value);
 
-// ----------------------------------
+//     }
+// }
+
 
